80cf8486248e920b96506ed8e98f563718e8d8fa
cafferidwan <quazi.hasib13@gmail.com>
2014-04-16 15:20:48 +0600

    2nd commit

--- Receiver/XMPPClient/src/org/apache/android/xmpp/Receiver.java
+++ Receiver/XMPPClient/src/org/apache/android/xmpp/Receiver.java
@@ -1,144 +1,57 @@
 package org.apache.android.xmpp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.speech.tts.TextToSpeech;
-import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
-import org.jivesoftware.smack.filter.MessageTypeFilter;
-import org.jivesoftware.smack.filter.PacketFilter;
-import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Packet;
-import org.jivesoftware.smack.util.StringUtils;
-
 import java.util.ArrayList;
-import java.util.Locale;
 
-public class Receiver extends Activity implements OnInitListener {
-
-    private ArrayList<String> messages = new ArrayList();
-    private Handler mHandler = new Handler();
-    private SettingsDialogReceiver mDialog;
-    private EditText mRecipient;
-    private EditText mSendText;
-    private ListView mList;
-    private XMPPConnection connection;
-    private TextToSpeech tts;
-    private String txt;
+public class Receiver extends Activity  
+{
+	//list for message
+    static ArrayList<String> messages = new ArrayList<String>();
+    //handler for posting message
+    static Handler mHandler = new Handler();
+    private static ListView mList;
+    //xmppconnection instance
+    public static XMPPConnection connection;
+    //creating xmppLogin instance for receiver login
+    public static XmppLogin receiverLogin;
+    //creating receiver instance
+    public static Receiver receive;
+    
     String DEBUG_TAG=Receiver.class.getSimpleName();
+    
+    
     
     @Override
     public void onCreate(Bundle icicle)
     {
         super.onCreate(icicle);
-        Log.i("XMPPClient", "onCreate called");
-        setContentView(R.layout.main);
+        Log.d(DEBUG_TAG, "onCreate called");
+        setContentView(R.layout.receiver_main);
+        receive = this;
 
-        tts=new TextToSpeech(this,this);
-        Log.i("XMPPClient", "mRecipient = " + mRecipient);
-        Log.i("XMPPClient", "mSendText = " + mSendText);
+        receiverLogin = new XmppLogin(this);
         
+        //start login connection using transmitter id
+        XmppLogin.SetLoginConnection("receiverid123@gmail.com", "receiver123", 2);
+        
+        //instantiating the list for setting messages in the list
         mList = (ListView) this.findViewById(R.id.listMessages);
-        Log.i("XMPPClient", "mList = " + mList);
+        Log.d(DEBUG_TAG, "mList = " + mList);
         setListAdapter();
 
-        mDialog = new SettingsDialogReceiver(this);
-        
-        Button setup = (Button) this.findViewById(R.id.setup);
-        setup.setOnClickListener(new View.OnClickListener() {
-            @Override
-			public void onClick(View view) {
-                
-                mDialog.onStart();
-            	
-            	
-            }
-        });
-
-        Button back=(Button)findViewById(R.id.back);
-        back.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-			
-				Intent vintent=new Intent(v.getContext(),MainActivity.class);
-				startActivityForResult(vintent, 0);
-				setResult(0);
-				finish();
-			}
-		});
     }
-
    
-    public void setConnection(XMPPConnection connection) 
+    //set the list adapter for multi line messages
+    public static void setListAdapter() 
     {
-        this.connection = connection;
-        if (connection != null) 
-        {
-            
-            PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
-            connection.addPacketListener(new PacketListener() 
-            {
-                @Override
-				public void processPacket(Packet packet) 
-                {
-                    Message message = (Message) packet;
-                    if (message.getBody() != null) 
-                    {
-                        String fromName = StringUtils.parseBareAddress(message.getFrom());
-                        Log.i("XMPPClient", "Got text [" + message.getBody() + "] from [" + fromName + "]");
-                        txt= message.getBody();
-                        Log.d(DEBUG_TAG, txt);
-                        if(txt.length()!=0){
-                        	String text=txt;
-                    		tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-                        	
-                        }
-                        
-                        messages.add(fromName + ":");
-                        messages.add(message.getBody());
-                        mHandler.post(new Runnable() 
-                        {
-                            @Override
-							public void run() 
-                            {
-                                setListAdapter();
-                            }
-                        });
-                    }
-                }
-            }, filter);
-        }
-    }
-
-    private void setListAdapter() 
-    {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.multi_line_list_item,messages);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(Receiver.receive, R.layout.multi_line_list_item_receiver,messages);
         mList.setAdapter(adapter);
     }
 
-
-	@Override
-	public void onInit(int status) {
-		// TODO Auto-generated method stub
-		if(status==TextToSpeech.SUCCESS){
-			int result=tts.setLanguage(Locale.ENGLISH);
-		
-		}else{
-			Log.e("TTS", "initialization failed");
-		}
-	}
-	
-	
-	
 }
---
+++ Receiver/XMPPClient/src/org/apache/android/xmpp/XmppLogin.java
@@ -0,0 +1,170 @@
+package org.apache.android.xmpp;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.MessageTypeFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.util.StringUtils;
+
+import android.util.Log;
+
+public class XmppLogin
+{
+	//creating transmitter instance
+	public static Transmitter xmppClientTransmitter;
+	//creating receiver instance
+	public static Receiver xmppClientReciver;
+	public static String DEBUG_TAG = XmppLogin.class.getSimpleName();
+	
+	//constructor for transmitter
+	public XmppLogin(Transmitter xmppClientPar)
+	{
+        XmppLogin.xmppClientTransmitter = xmppClientPar;
+	}
+
+	//constructor for receiver
+	public XmppLogin(Receiver xmppClientReciver)
+	{
+        XmppLogin.xmppClientReciver = xmppClientReciver;
+	}
+
+	//start login when the app starts
+	public static void SetLoginConnection(String userName, String passWord, int Number)
+	{
+		String host = "talk.google.com";
+		String port = "5222";
+		String service = "gmail.com";
+		String username = userName;
+		String password = passWord;
+		//number=1 is for Transmitter login and number=2 is for receiver login
+		int number = Number;
+
+		// Create a connection
+		ConnectionConfiguration connConfig = new org.jivesoftware.smack.ConnectionConfiguration(
+				host, Integer.parseInt(port), service);
+		// new ConnectionConfiguration(host, Integer.parseInt(port), service);
+		XMPPConnection connection = new XMPPConnection(connConfig);
+
+		try 
+		{
+			connection.connect();
+			Log.d(DEBUG_TAG, "[SettingsDialogPar] Connected to "
+					+ connection.getHost());
+		}
+		catch (XMPPException ex) 
+		{
+			Log.d(DEBUG_TAG, "[SettingsDialogPar] Failed to connect to "
+					+ connection.getHost());
+			Log.d(DEBUG_TAG, ex.toString());
+			
+			if(number==1)
+			{
+				setTransmitterConnection(null);
+			}
+			else if(number==2)
+			{
+				setReceiverConnection(null);
+			}
+		}
+		
+		try 
+		{
+			connection.login(username, password);
+			Log.d(DEBUG_TAG, "Logged in as " + connection.getUser());
+
+			// Set the status to available
+			Presence presence = new Presence(Presence.Type.available);
+			connection.sendPacket(presence);
+			if(number==1)
+			{
+				setTransmitterConnection(connection);
+			}
+			else if(number==2)
+			{
+				setReceiverConnection(connection);
+			}
+			
+		}
+		catch (XMPPException ex) 
+		{
+			Log.d(DEBUG_TAG, "[SettingsDialogPar] Failed to log in as "
+					+ username);
+			Log.d(DEBUG_TAG, ex.toString());
+			
+			if(number==1)
+			{
+				setTransmitterConnection(null);
+			}
+			else if(number==2)
+			{
+				setReceiverConnection(null);
+			}
+		}
+	}
+	
+	//set connection for receiver; after login, it will only receive text
+	public static void setReceiverConnection(XMPPConnection connection)
+	{
+		Receiver.connection = connection;
+		if (connection != null)	
+		{
+			// Add a packet listener to get messages sent to us
+			PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
+			connection.addPacketListener(new PacketListener() 
+			{
+				public void processPacket(Packet packet) 
+				{
+					Message message = (Message) packet;
+					if (message.getBody() != null) 
+					{
+						String fromName = StringUtils.parseBareAddress(message.getFrom());
+							Log.d(DEBUG_TAG, "Got text [" + message.getBody()+ "] from [" + fromName + "]");
+						Receiver.messages.add(fromName + ":");
+						Receiver.messages.add(message.getBody());
+						// Add the incoming message to the list view
+						Receiver.mHandler.post(new Runnable() 
+						{
+							public void run()
+							{
+								Receiver.setListAdapter();
+							}
+						});
+					}
+				}
+			}, filter);
+		}
+	}
+	
+	//set connection for transmitter; after login, it will only send text
+	public static void setTransmitterConnection(XMPPConnection connection)
+	{
+		Transmitter.connection = connection;
+		if (connection != null)
+		{
+			// Add a packet listener to get messages sent to us
+			PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
+			connection.addPacketListener(new PacketListener() 
+			{
+				public void processPacket(Packet packet) 
+				{
+					Message message = (Message) packet;
+					if (message.getBody() != null) 
+					{
+						String fromName = StringUtils.parseBareAddress(message
+								.getFrom());
+						Log.d(DEBUG_TAG, "Got text [" + message.getBody()+ "] from [" + fromName + "]");
+
+					}
+				}
+			}, filter);
+		}
+	}
+	
+	
+	
+}
